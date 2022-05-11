40c96d6c1febac75c5fa03fc2ce669653f503bec
Fernando Granados <esleydergranados@gmail.com>
2022-05-10 20:51:36 -0500

    Fix blank url

--- DigitalMonster/DigitalMonster/urls.py
+++ DigitalMonster/DigitalMonster/urls.py
@@ -3,8 +3,12 @@
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+
+
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/monsters/')),
     path('admin/', admin.site.urls),
     path('monsters/', include("monsters.urls"))
 ]
